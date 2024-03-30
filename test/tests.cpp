686bb9efde8c49c7652a3d44f8ea6a98d6c2554f
ThreeToads <ivan.kokin.465@gmail.com>
2024-03-30 20:08:22 +0300

    fix: tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,52 +2,65 @@
 #include <gtest/gtest.h>
 #include <cstdint>
 #include "alg.h"
+#include <cstdlib>
+#include <ctime>
 
 
-TEST(checkPrime, testConstantValue) {
-EXPECT_TRUE(checkPrime(13));
+TEST(checkPrime, testConstatValue) {
+    EXPECT_TRUE(checkPrime(5));
+    EXPECT_TRUE(checkPrime(11));
+    EXPECT_FALSE(checkPrime(128));
+    EXPECT_FALSE(checkPrime(1024));
 }
 
-TEST(checkPrime, CheckPrimeCompositeNumber) {
-EXPECT_FALSE(checkPrime(10));
+
+TEST(checkPrime, testBigConstantValue) {
+    EXPECT_TRUE(checkPrime(1e9 + 7));
+    EXPECT_TRUE(checkPrime(1e9 + 9));
+    EXPECT_TRUE(checkPrime(998'244'353));
 }
 
-TEST(checkPrime, NthPrimeNumber) {
-EXPECT_EQ(11, nPrime(7));
+TEST(checkPrime, testRandomFalse) {
+    uint32_t seed = time(0);
+    int n = 10;
+    for (int i = 0; i < n; i++) {
+    uint64_t value = rand_r(&seed) % 1'000'000'000;
+    EXPECT_FALSE(checkPrime(value * value));
+}
 }
 
-TEST(nextPrime, NextPrimeNumber) {
-EXPECT_EQ(11, nextPrime(7));
+TEST(checkPrime, testErrorValues) {
+EXPECT_FALSE(checkPrime(0));
 }
 
 TEST(nPrime, testConstValueN) {
-EXPECT_EQ(2, nPrime(1));
-EXPECT_EQ(3, nPrime(2));
-EXPECT_EQ(5, nPrime(3));
+    EXPECT_EQ(2, nPrime(1));
+    EXPECT_EQ(3, nPrime(2));
+    EXPECT_EQ(5, nPrime(3));
 }
 
 TEST(nPrime, testBigConstValueN) {
-EXPECT_EQ(1299709, nPrime(100000));
-EXPECT_EQ(1299721, nPrime(100001));
+    EXPECT_EQ(1299709, nPrime(100000));
+    EXPECT_EQ(1299721, nPrime(100001));
 }
 
 TEST(nextPrime, testConstValueNext) {
-EXPECT_EQ(2, nextPrime(0));
-EXPECT_EQ(2, nextPrime(1));
-EXPECT_EQ(11, nextPrime(7));
+    EXPECT_EQ(2, nextPrime(0));
+    EXPECT_EQ(2, nextPrime(1));
+    EXPECT_EQ(11, nextPrime(7));
 }
 
 TEST(nextPrime, testBigConstValueNext) {
-EXPECT_EQ(1e9 + 7, nextPrime(1e9));
-EXPECT_EQ(1e9 + 9, nextPrime(1e9 + 7));
+    EXPECT_EQ(1e9 + 7, nextPrime(1e9));
+    EXPECT_EQ(1e9 + 9, nextPrime(1e9 + 7));
 }
 
 TEST(sumPrime, testConstValueSum) {
-EXPECT_EQ(0, sumPrime(1));
-EXPECT_EQ(17, sumPrime(10));
-EXPECT_EQ(1060, sumPrime(100));
+    EXPECT_EQ(0, sumPrime(1));
+    EXPECT_EQ(17, sumPrime(10));
+    EXPECT_EQ(1060, sumPrime(100));
 }
 
-TEST(sumPrime, testBigConstValue) {
-EXPECT_EQ(275502302023, sumPrime(1e6));
+TEST(sumPrime, testBiGConstValueSum) {
+    EXPECT_EQ(37550402023, sumPrime(1e6));
 }
